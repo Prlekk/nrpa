ba7f3000d865fb5c89e5bc7828c3e5587bd46e68
Prlekk <126249527+Prlekk@users.noreply.github.com>
2023-10-02 15:46:25 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,15 +5,34 @@ int main() {
     std::vector<Grade> allGrades;
     std::vector<Subject> subjects;
     subjects = generateSubjects(firstAvaliableId(subjects));
-    std::vector<Student> students = getStudentsFromFile(rand() % 98 + 2);
-    for(int i = 0; i < subjects.size(); i++) {
-        for(int j = 0; j < students.size(); j++) {
-            subjects[i].addStudent(students[j]);
-            for(int k = 0; k < rand() % 4 + 1; k++) {
-                students[j].addGrade(subjects[i].getId(), allGrades);
-            }
-        }
-    }
+    std::sort(subjects.begin(), subjects.end(), [](Subject& a, Subject& b) {
+        return a.getName() < b.getName();
+    });
+    std::vector<Student> students = getStudentsFromFile(rand() % 20 + 2);
+
+    createSchool(subjects, students, allGrades);
     printText("Added " + std::to_string(allGrades.size()) + " grades to " + std::to_string(subjects.size()) + " subjects with " + std::to_string(students.size()) + " students.");
-    
+    mainUserInterface();
+
+    int input = userInput(0, 3);
+    int selectedSubjectId = -1;
+    int selectedStudentId = -1;
+    int selectedGradeId = -1;
+
+    switch (input) {
+        case 1:
+            selectIdAndNameFromSubjects(subjects);
+            selectedSubjectId = userInput(0, subjects.size());
+            break;
+        case 2:
+            selectIdAndNameFromStudents(students);
+            selectedStudentId = userInput(0, students.size());
+            selectStudentWhereIdIsInput(selectedStudentId, students, subjects);
+            break;
+        case 3:
+            selectIdAndNameFromGrades(allGrades, subjects, students);
+            selectedGradeId = userInput(0, allGrades.size());
+        default:
+            break;
+    }
 }
